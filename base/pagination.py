f24b7633578208da5c77e9d6ea0d22a2c98169c8
adityabhalsod <adityabhalsod8@gmail.com>
2022-03-28 22:40:54 +0530

    Update.

--- base/pagination.py
+++ base/pagination.py
@@ -6,6 +6,6 @@ from rest_framework.pagination import PageNumberPagination
 
 class Pagination(PageNumberPagination):
     page_size = 10
+    page_query_param = "offset"
     page_size_query_param = "limit"
     max_page_size = 100
-
